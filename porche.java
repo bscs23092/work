7785a23dbd4b055bf11f2619278d9ba063cf20c2
mohid685 <mohidx186@gmail.com>
2025-03-13 00:07:45 +0500

    change by isl

--- porche.java
+++ porche.java
@@ -5,6 +5,7 @@ public class porche{
     public static void main(String[] args) {
 
         System.out.println("GTR IS BETTER AND FASTER THAN PORCHE!!");
+        System.out.println("PAUL WALKER HAD A SKYLINE WHICH IS ALSO MY FAVOURITE");
 
     }
 
