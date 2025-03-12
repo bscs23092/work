a5877d8cb3b6f9a48a1b7935b0e6ade8c712c1e2
mohid685 <mohidx186@gmail.com>
2025-03-13 00:06:14 +0500

    change by lgs

--- porche.java
+++ porche.java
@@ -4,7 +4,8 @@ public class porche{
 
     public static void main(String[] args) {
 
-        System.out.println("GTR IS BETTER AND FASTER THAN PORCHE!!");
+        System.out.println("GTR IS NOT BETTER THAN PORCHE , I DISAGREE!!");
+        System.out.println("PORCHE IS MUCH BETTER IN TERMS OF SPEED AND COMFORT!!!");
 
     }
 
