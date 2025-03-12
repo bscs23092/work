929bc58b9a5e7d6ec3ecc804706845f92dcd3a6f
mohid685 <mohidx186@gmail.com>
2025-03-12 23:35:18 +0500

    initial commit

---
+++ gtr.java
@@ -0,0 +1,15 @@
+package work;
+
+import java.util.*;
+
+
+public class gtr{
+
+    public static void main(String[] args) {
+
+        System.out.println("Preparing for mid exam ...");
+
+    }
+
+}
+
