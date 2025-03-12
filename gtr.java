1470eab6067b7b217053d3802a930ec781f92501
mohid685 <mohidx186@gmail.com>
2025-03-12 23:59:34 +0500

    added a new comment

--- gtr.java
+++ gtr.java
@@ -8,6 +8,7 @@ public class gtr{
     public static void main(String[] args) {
 
         System.out.println("Preparing for mid exam ...");
+        System.out.println("you will ace this exam for sure ...");
 
     }
 
